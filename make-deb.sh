5a6827fb1bcbc2c3f405877f7a733a5ef5233724
Jonathan Blakes <jvb@cs.nott.ac.uk>
2011-06-21 16:13:38 +0100

    Change-Id: Ifa3e1876b0e85eef66ed230f15f0d96976fd2a74

--- make-deb.sh
+++ make-deb.sh
@@ -1,16 +1,2 @@
-#/bin/sh
-# http://www.pytips.com/2010/5/24/how-to-easily-packags-python-softwares-for-ubuntu
-export PYTHONPATH=`pwd`:$PYTHONPATH
-VERSION=`python -c "import quantities; print quantities.__version__,"`
-which py2dsc || { echo "stdeb not installed. Run 'sudo apt-get install python-stdeb' before retrying."; exit; }
-./clean.sh
-python setup.py sdist &&
-cd dist &&
-py2dsc quantities-$VERSION.tar.gz &&
-cd deb_dist &&
-cp ../../quantities/constants/NIST_codata.txt quantities-$VERSION/quantities/constants/ &&
-cd quantities-$VERSION &&
-debuild
-cd ..
-echo
-echo "Run 'sudo dpkg -i `pwd`/python-quantities_$VERSION-1_all.deb' to install."
+#!/bin/sh
+python setup.py --command-packages=stdeb.command sdist_dsc --no-backwards-compatibility bdist_deb && dpkg -I deb_dist/python-quantities_0.9.0-1_all.deb
